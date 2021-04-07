b5aa1848e7012e95c16791a80a78b6e2a7cd1714
Mauro Song <maurosong@protonmail.com>
2021-04-07 14:08:18 -0300

    Fix usermod groupmod

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 
 # coincidir con uid gid del user en el host
 if ! [ -z "$MAP_UID" ]; then
-    usermod -u user $MAP_UID
+    usermod -u $MAP_UID user 
 fi
 
 if ! [ -z "$MAP_GID" ]; then
-    groupmod -u user $MAP_GID
+    groupmod -g $MAP_GID user 
 fi
 
 su user -c "jupyter-lab --ip 0.0.0.0 --LabApp.token=''"
